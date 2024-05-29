536b0bcc2d68a5da22183291b0c893933dc24497
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-05-29 16:59:15 +0200

    sort in ordine alfabetico

--- first-react-app/src/List.jsx
+++ first-react-app/src/List.jsx
@@ -3,9 +3,11 @@ function List() {
     { id: 2, name: 'pera', calories: 40 },
     { id: 3, name: 'banana', calories: 105 },
     { id: 4, name: 'cocco', calories: 150 }];
-    fruits.sort();
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}</li>)
+    //sort in ordine alfabetico.
+    fruits.sort((a, b) => a.name.localeCompare(b.name));
+
+    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}, calories: {fruit.calories}</li>)
 
     return (<ul>{listItems}</ul>)
 }
