1dd5a1d79bf10cb33b20497c26b2f542219c9241
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-06-05 11:22:36 +0200

    if in react component

--- first-react-app/src/Button.jsx
+++ first-react-app/src/Button.jsx
@@ -1,8 +1,23 @@
 function Button() {
+    //realizziamo un counter per la creazione di una condition nella funzione
+    let count = 0;
+
 
     const handleClick = () => console.log('clicked');
 
-    const handleClick2 = (name) => console.log(`${name} stop clicking me `)
+    const handleClick2 = (name) => console.log(`${name} stop clicking me `);
+
+    const handleClick3 = (name) => {
+        if (count < 3) {
+            count++;
+            console.log(`${name} mi hai cliccato ${count} volta/e`)
+        } else {
+            console.log(`${name} smettila di cliccare`)
+        }
+
+
+
+    }
 
 
     //in questo modo, la funzione viene chiamata direttamente senza attendere il click del bottone
@@ -12,7 +27,7 @@ function Button() {
 
     //in questo modo la funzione viene chiamata al click
     return (
-        <button onClick={() => handleClick2('pier')}>my button</button>
+        <button onClick={() => handleClick3('pier')}>my button</button>
     );
 }
 
