7993139873cc31d386a1bd0f535f41aa4e6fdbec
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-06-05 11:03:45 +0200

    onclick function with error and correction

--- first-react-app/src/Button.jsx
+++ first-react-app/src/Button.jsx
@@ -1,9 +1,18 @@
 function Button() {
 
+    const handleClick = () => console.log('clicked');
+
+    const handleClick2 = (name) => console.log(`${name} stop clicking me `)
 
 
+    //in questo modo, la funzione viene chiamata direttamente senza attendere il click del bottone
+    // return (
+    //     <button onClick={handleClick2('pier')}>my button</button>
+    // );
+
+    //in questo modo la funzione viene chiamata al click
     return (
-        <button>my button</button>
+        <button onClick={() => handleClick2('pier')}>my button</button>
     );
 }
 
